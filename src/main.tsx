1c8db5405f931d4c1afa72c3fc5315c9ab7a165b
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-29 10:15:28 +0700

    Chore: Configure UI and Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import Homepage from "@/pages/Homepage";
+import Homepage from "@/pages/homepage";
 import "@/styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
