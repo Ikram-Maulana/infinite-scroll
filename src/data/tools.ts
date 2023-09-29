2e38492b065dbcaf9a57235867a2f040695e7a58
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-29 16:04:05 +0700

    Chore: Tidy Up Tools Data

---
+++ src/data/tools.ts
@@ -0,0 +1,76 @@
+import blenderLogo from "@/assets/blender-logo.svg";
+import excelLogo from "@/assets/excel-logo.svg";
+import figmaLogo from "@/assets/figma-logo.svg";
+import flutterLogo from "@/assets/flutter-logo.svg";
+import golangLogo from "@/assets/golang-logo.svg";
+import kotlinLogo from "@/assets/kotlin-logo.svg";
+import laravelLogo from "@/assets/laravel-logo.svg";
+import pythonLogo from "@/assets/python-logo.svg";
+import reactLogo from "@/assets/react-logo.svg";
+import vueLogo from "@/assets/vue-logo.svg";
+
+export const tools1 = [
+  {
+    name: "Figma",
+    link: "https://figma.com",
+    logo: figmaLogo,
+    description: "UI/UX Design",
+  },
+  {
+    name: "Flutter",
+    link: "https://flutter.dev",
+    logo: flutterLogo,
+    description: "Multiplatform Development",
+  },
+  {
+    name: "Laravel",
+    link: "https://laravel.com",
+    logo: laravelLogo,
+    description: "Backend Development",
+  },
+  {
+    name: "Vue",
+    link: "https://vuejs.org",
+    logo: vueLogo,
+    description: "Frontend Development",
+  },
+  {
+    name: "Python",
+    link: "https://python.org",
+    logo: pythonLogo,
+    description: "Data Science",
+  },
+];
+
+export const tools2 = [
+  {
+    name: "Blender",
+    link: "https://blender.org",
+    logo: blenderLogo,
+    description: "3D Modeling",
+  },
+  {
+    name: "Excel",
+    link: "https://microsoft.com/excel",
+    logo: excelLogo,
+    description: "Data Analysis",
+  },
+  {
+    name: "Golang",
+    link: "https://golang.org",
+    logo: golangLogo,
+    description: "Backend Development",
+  },
+  {
+    name: "Kotlin",
+    link: "https://kotlinlang.org",
+    logo: kotlinLogo,
+    description: "Android Development",
+  },
+  {
+    name: "React",
+    link: "https://reactjs.org",
+    logo: reactLogo,
+    description: "Frontend Development",
+  },
+];
