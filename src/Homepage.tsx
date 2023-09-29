66f7728bc15f90ffa55852a33264c18080298239
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-29 09:51:59 +0700

    Chore: Configure Tailwind CSS

---
+++ src/Homepage.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+const Homepage: FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugiat
+        distinctio nihil modi id omnis numquam quisquam nostrum optio laudantium
+        totam, a facilis, nobis, officiis minus repellendus illum eligendi alias
+        sed. Omnis, voluptate dicta rerum perspiciatis eos magnam quae ipsam
+        dolorem nulla excepturi alias aliquid dolorum accusantium fugit ab
+        facere ratione expedita velit quod praesentium, fugiat inventore rem
+        cumque? Natus laboriosam iste voluptas voluptate placeat libero eligendi
+        maxime tempore id dolore magnam esse minus quibusdam qui, sapiente est
+        iusto voluptatum. Sunt expedita voluptatem quasi iusto magnam nemo
+        suscipit, illo optio enim quia officiis! Adipisci minima deserunt
+        aliquid. Quod, corrupti exercitationem!
+      </p>
+    </>
+  );
+};
+
+export default Homepage;
