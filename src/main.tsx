1c29efb31af75f2f9eadb5fe5d1dc016252103a0
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-29 09:55:57 +0700

    Chore: Configure Aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import Homepage from "@/pages/Homepage";
+import "@/styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Homepage from "./Homepage.tsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
